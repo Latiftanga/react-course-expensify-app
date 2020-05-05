c91f264014af94674729448b8b62bae50a71d7c3
Latif Adams <latiftanga@gmail.com>
2020-05-05 17:14:07 +0000

    Remove dummy data for expenses

--- src/index.js
+++ src/index.js
@@ -9,15 +9,6 @@ import * as serviceWorker from './serviceWorker';
 
 const store = configStore();
 
-store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
-store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 109500}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
-
-console.log(visibleExpenses);
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
